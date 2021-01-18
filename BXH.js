c2880dec659c5a10ecbab561479591146628d356
ezreall <huythang1019@gmail.com>
2021-01-18 08:29:11 +0700

    baitap

---
+++ BXH.js
@@ -0,0 +1,10 @@
+class BXH{
+    constructor(ingame,number,point,ratio,img) {
+        this.ingame=ingame;
+        this.number=number;
+        this.point=point;
+        this.ratio=ratio;
+        this.img=img;
+    }
+
+}
