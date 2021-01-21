64cf319bbd47dd82f48e0c63174cafc5a0ea621f
ezreall <huythang1019@gmail.com>
2021-01-21 22:20:33 +0700

    baitap

--- manage.js
+++ manage.js
@@ -92,3 +92,22 @@ function loadData(){
     }
 
 }
+function myFunction() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("manage");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
